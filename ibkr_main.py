0414e9cfed31be1f1fb22ddab4cf81a3e867c2e2
Robert Castagna <49927397+RobertCastagna@users.noreply.github.com>
2024-09-22 15:22:16 -0400

    Class/ Method/ Function docstrings & etc.

--- ibkr_main.py
+++ ibkr_main.py
@@ -6,11 +6,12 @@ from ibapi.client import EClient
 from ibapi.common import BarData, TickerId
 from ibapi.wrapper import EWrapper
 from ibapi.contract import Contract, ContractDetails
-import threading
+from threading import Thread
 import time
-
+from typing import Any
 
 class IBApi(EWrapper, EClient):
+    """EWrapper handles the receiving of data, EClient is sending"""
     def __init__(self, data_queue):
         EClient.__init__(self, self)
         self.data_queue = data_queue
@@ -19,14 +20,15 @@ class IBApi(EWrapper, EClient):
         print('Timestamp: ', headTimestamp)
         return headTimestamp
 
-    def cancelHeadTimeStamp(self, reqId: int):
-        return super().cancelHeadTimeStamp(reqId)
+    ## Dont really need to implement these because we do not override the super class 
+    # def cancelHeadTimeStamp(self, reqId: int):
+    #     return super().cancelHeadTimeStamp(reqId)
 
-    def contractDetails(self, reqId: TickerId, contractDetails: ContractDetails):
-        return super().contractDetails(reqId, contractDetails)
+    # def contractDetails(self, reqId: TickerId, contractDetails: ContractDetails):
+    #     return super().contractDetails(reqId, contractDetails)
     
-    def contractDetailsEnd(self, reqId: TickerId):
-        return super().contractDetailsEnd(reqId)
+    # def contractDetailsEnd(self, reqId: TickerId):
+    #     return super().contractDetailsEnd(reqId)
 
     def historicalData(self, reqId: TickerId, bar: BarData):
         self.data_queue.put(bar)
@@ -37,18 +39,24 @@ class IBApi(EWrapper, EClient):
         self.data_queue.put(None)
         return super().historicalDataEnd(reqId, start, end)
     
-def run_loop(api_client):
+def run_loop(api_client: IBApi):
+    """Target thread for api event loop."""
     api_client.run()
 
-def start_client(data_queue):
+def start_client(data_queue: Queue):
+    """Starts the tws api in a seperate thread that will not be touched in rest of program."""
     api_client = IBApi(data_queue)
     api_client.connect('127.0.0.1', 7497, clientId=1)
-    api_thread = threading.Thread(target=run_loop, args=(api_client,), daemon=True)
+    api_thread = Thread(target=run_loop, args=(api_client,), daemon=True)
     api_thread.start()
     time.sleep(1)
     return api_client, api_thread
 
-def close_client(api_client, data_queue, processing_thread, api_thread):
+def close_client(api_client: ibapi, data_queue: Queue, processing_thread: Thread, api_thread: Thread):
+    """
+    Safely closes client by issuing stop to queue processing, joining api thread, client thread and disconnecting
+    from api to unblock resources.
+    """
     data_queue.put(None)
     processing_thread.join()
 
@@ -56,7 +64,8 @@ def close_client(api_client, data_queue, processing_thread, api_thread):
     api_thread.join()
     print("Safely Exited Connection.")
 
-def data_processing_thread(data_queue):
+def data_processing_thread(data_queue: Queue):
+    """When this is called, it will do stuff to the data in the queue until seeing a None value."""
     while True:
         data = data_queue.get()
         if data is None:
@@ -65,12 +74,14 @@ def data_processing_thread(data_queue):
         process_data(data)
         data_queue.task_done()
 
-def process_data(data):
+def process_data(data: Any):
+    """Future implementation required."""
     # do whatever
     time.sleep(2)
     print("Data Processed.")
     
-def create_contract(ticker):
+def create_contract(ticker: str):
+    """Only supports stocks."""
     contract = Contract()
     contract.symbol = ticker
     contract.secType = 'STK'
@@ -80,17 +91,18 @@ def create_contract(ticker):
 
 if __name__ == '__main__':
     try:
-        # main program logic
         data_queue = Queue() 
         api_client, api_thread = start_client(data_queue)
 
-        processing_thread = threading.Thread(target=data_processing_thread, args=(data_queue,))
+        # start the seperate thread for processing the resulting data getting dropped in the Queue
+        processing_thread = Thread(target=data_processing_thread, args=(data_queue,))
         processing_thread.start()
 
+        # main program logic
         appl_contract = create_contract('AAPL')
         api_client.reqHeadTimeStamp(1, appl_contract, "TRADES", 1, 1)
         api_client.reqHistoricalData(1, appl_contract, '', '1 M', '1 day', 'MIDPOINT', 0, 1, False, [])
-        
+
         while True:
             pass
 
